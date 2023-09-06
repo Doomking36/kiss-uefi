8669985304c680617abfafefc27d52fb90ab6035
Nathan Nguyen <31893431+Doomking36@users.noreply.github.com>
2023-09-05 19:19:15 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-yes | pacman -Sy tar curl xz
-curl --output kiss.xz https://codeberg.org/kiss-community/repo/releases/download/23.04.30/kiss-chroot-23.04.30.tar.xz
-tar xf kiss.xz -C /mnt
+yes | pacman -Sy tar wget xz
+wget https://codeberg.org/kiss-community/repo/releases/download/23.04.30/kiss-chroot-23.04.30.tar.xz
+tar xf kiss-chroot-23.04.30.tar.xz -C /mnt
 cp chroot.sh /mnt
 cp profile /mnt
 
